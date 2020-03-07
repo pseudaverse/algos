6c43267bc53ed2e530706c8e280118027a031eac
pseudaverse <kamil111998@gmail.com>
2020-03-07 04:59:09 +0300

    Update segtree_sum.cpp

--- segtree_sum.cpp
+++ segtree_sum.cpp
@@ -1,62 +1,54 @@
 struct segtree {
-	// меняем
+	// 4Head
 	struct node {
-		node(ll x) :val(x) {};
-		node() : val(0) {};
-		ll val = 0;
-		ll lazy = 0;
-		bool is_lazy = 0;
+		node(ll x) : val(x) {};
+		node() : val(0) {}; // neutral
+		ll val = 0; // neutral
+		ll lazy = 0; // -1
 		void apply(int tl, int tr, ll add) {
-			//val += (tr - tl + 1) * add;
-			val = (tr - tl + 1) * add;
-			lazy = add;
-			is_lazy = 1;
+			val += add * (tr - tl + 1); // = add
+			lazy += add; // = add
 		}
 	};
-	node neutral;
+	void push(int u, int tl, int tr) {
+		if (t[u].lazy != 0 /* -1 */) {
+			int tm = (tl + tr) / 2;
+			t[u * 2].apply(tl, tm, t[u].lazy);
+			t[u * 2 + 1].apply(tm + 1, tr, t[u].lazy);
+			t[u].lazy = 0; // -1
+		}
+	}
 	node merge(const node& a, const node& b) const {
-		//return node(min(a.val_mn, b.val_mn), max(a.val_mx, b.val_mx));
 		return node(a.val + b.val);
 	}
-	// дальше не трогай
-	void push(int u, int tl, int tr) {
-		if (t[u].is_lazy) {
-			int tm = (tl + tr) / 2;
-			if (tl != tr) {
-				t[u * 2].apply(tl, tm, t[u].lazy);
-				t[u * 2 + 1].apply(tm + 1, tr, t[u].lazy);
-			}
-			t[u].is_lazy = 0;
-		}
-	}
+	// 4Head
 	node get(int u, int tl, int tr, int l, int r) {
-		if (r < l || tr < tl || tl > r || tr < l) {
+		if (r < l || tl > r || tr < l) {
 			return neutral;
 		}
-		push(u, tl, tr);
 		if (l <= tl && tr <= r) {
 			return t[u];
 		} else {
+			push(u, tl, tr);
 			int tm = (tl + tr) / 2;
 			return merge(get(u * 2, tl, tm, l, r), get(u * 2 + 1, tm + 1, tr, l, r));
 		}
 	}
 	template<typename... M>
 	void upd(int u, int tl, int tr, int l, int r, const M&... m) {
-		if (r < l || tr < tl || tl > r || tr < l) {
+		if (r < l || tl > r || tr < l) {
 			return;
 		}
-		push(u, tl, tr);
 		if (l <= tl && tr <= r) {
 			t[u].apply(tl, tr, m...);
 		} else {
+			push(u, tl, tr);
 			int tm = (tl + tr) / 2;
 			upd(u * 2, tl, tm, l, r, m...);
 			upd(u * 2 + 1, tm + 1, tr, l, r, m...);
 			t[u] = merge(t[u * 2], t[u * 2 + 1]);
 		}
 	}
- 
 	template<class T>
 	void build(int u, int tl, int tr, vector<T>& v) {
 		if (tl == tr) {
@@ -69,12 +61,19 @@ struct segtree {
 			t[u] = merge(t[u * 2], t[u * 2 + 1]);
 		}
 	}
- 
-	template<class T>
-	segtree(int n) : n(n) {
+	void casual_build(int u, int tl, int tr) {
+		if (tl == tr) {
+			return;
+		} else {
+			int tm = (tl + tr) / 2;
+			casual_build(u * 2, tl, tm);
+			casual_build(u * 2 + 1, tm + 1, tr);
+			t[u] = merge(t[u * 2], t[u * 2 + 1]);
+		}
+	}
+	segtree(ll n) : n(n) {
 		t.resize(4 * n);
-		vector<T> v(n, neutral);
-		build(1, 0, n - 1, v);
+		casual_build(1, 0, n - 1);
 	}
 	template<class T>
 	segtree(vector<T>& v) : n(v.size()) {
@@ -88,7 +87,7 @@ struct segtree {
 	void upd(int l, int r, const M&... m) {
 		upd(1, 0, n - 1, l, r, m...);
 	}
- 
 	int n;
 	vector<node>t;
+	node neutral;
 };
